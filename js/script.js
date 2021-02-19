6ffdba6d40fed109c048de0acdb0058cb96e1f2e
de1an <dejan.lukic98@gmail.com>
2021-02-19 14:43:15 +0100

    hamb meni

--- js/script.js
+++ js/script.js
@@ -1,50 +1,45 @@
-$(document).ready(function() {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    autoplaySpeed: 1000,
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 1
-      },
-      1000: {
-        items: 1
-      }
-    }
-  });
-
-  $(window).on('scroll', function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 100) {
-      $('.sticky').addClass('stickyAdd');
-    } else {
-      $('.sticky').removeClass('stickyAdd');
-    }
-  });
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplaySpeed: 1000,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
     
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 100) {
+            $('.sticky').addClass('stickyAdd');
+        } else {
+            $('.sticky').removeClass('stickyAdd');
+        }
+    });
 
-//  $('.cena').mouseenter(function () {
-//        $(this).css({
-//            border: "0px solid #f85a16"
-//        }).animate({
-//            borderWidth: '5px',
-//        }, 200);
-//    }).mouseleave(function () {
-//        $(this).animate({
-//             borderWidth: '0px',
-//        }, 500);
-//    });
 });
 
-    function toggleDropdown() {
-        let navbarToggle = document.getElementById('navbar-toggle');
-        if(navbarToggle.className === 'topNav') {
-            navbarToggle.className += ' responsive';
-        } else {
-            navbarToggle.className = 'topNav';
-        }
+    const mainMenu = document.querySelector('.mainMenu');
+    const closeMenu = document.querySelector('.closeMenu');
+    const openMenu = document.querySelector('.openMenu');
+
+    openMenu.addEventListener('click', show);
+    closeMenu.addEventListener('click', close);
+
+    function show() {
+        mainMenu.style.display = 'flex';
+        mainMenu.style.top = '0';
+    }
+
+    function close() {
+        mainMenu.style.top = '-100%';
     }
